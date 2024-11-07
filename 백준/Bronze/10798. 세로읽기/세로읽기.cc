0fc45c2321ba2b27107a4702c3f49b38d84c4d15
jeongyujin03 <102495547+jeongyujin03@users.noreply.github.com>
2024-11-07 13:40:28 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.cc
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -1,38 +1,19 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 using namespace std;
 
 int main(void){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    string words[5];
 
-    vector<string> words(5);
-    vector<char> v;
-    int maxIdx = 0;
     for(int i=0; i<5; i++){
         cin >> words[i];
-        maxIdx = max(int(words[i].size()), maxIdx);
     }
 
-    for(int i=0; i<5; i++){
-        while(int(words[i].size()) < maxIdx){
-            words[i].push_back(' ');
-        }
-    }
-
-    for(int i=0; i<maxIdx; i++){
+    for(int i=0; i<15; i++){
         for(int j=0; j<5; j++){
-            char c = words[j].at(i);
-            if(c != ' '){
-                v.push_back(c);
+            if(i < words[j].size()){
+                cout << words[j][i];
             }
         }
-    }
-
-    for(int i=0; i<v.size(); i++){
-        cout << v[i];
     }
     return 0;
 }
