7f6629251d871dbd69d83c86b713e68214e87ca0
jeongyujin03 <102495547+jeongyujin03@users.noreply.github.com>
2024-11-07 12:08:24 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,42 +1,27 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
 
 int main(void){
-    vector<int> tall;
+    int tall[9];
     int sum = 0;
-    int erase1, erase2;
-    int x;
-
+    
     for(int i=0; i<9; i++){
-        cin >> x;
-        tall.push_back(x);
-        sum += x;
+        cin >> tall[i];
+        sum += tall[i];
     }
 
-    bool isres = false;
+    sort(tall, tall + 9);
     for(int i=0; i<9; i++){
         for(int j=i+1; j<9; j++){
-            int res = sum;
-            if(i != j){
-                res -= tall[i] + tall[j];
-                if(res == 100){
-                    erase1 = tall[i];
-                    erase2 = tall[j];
-                    isres = true;
-                    break;
+            if(sum - (tall[i] + tall[j]) == 100){
+                for(int k=0; k<9; k++){
+                    if(k != i && k != j){
+                        cout << tall[k] << "\n";
+                    }
                 }
+                return 0;
             }
         }
-        if(isres) break;
     }
-
-    sort(tall.begin(),tall.end());
-    for(int i=0; i<tall.size(); i++){
-        if(tall[i] != erase1 && tall[i] != erase2){
-            cout << tall[i] << "\n";
-        }
-    }
-    return 0;
 }
