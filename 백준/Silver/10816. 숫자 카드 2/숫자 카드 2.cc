b44845aea7487b566572678f6a8de976b8ca0234
jeongyujin03 <102495547+jeongyujin03@users.noreply.github.com>
2024-12-05 16:00:01 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 292 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -3,55 +3,25 @@
 #include <algorithm>
 using namespace std;
 
-int lower_bound(vector<int>& v, int target);
-int upper_bound(vector<int>& v, int target);
-
 int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-
-    int N, M, X;
-    vector<int> card;
+    
+    int N, X, M;
+    vector<int> v;
     cin >> N;
     for(int i=0; i<N; i++){
         cin >> X;
-        card.push_back(X);
+        v.push_back(X);
     }
-    sort(card.begin(), card.end());
+    sort(v.begin(), v.end());
 
     cin >> M;
     for(int i=0; i<M; i++){
         cin >> X;
-        cout << upper_bound(card, X) - lower_bound(card, X) << " ";
+        cout << upper_bound(v.begin(), v.end(), X)
+        - lower_bound(v.begin(), v.end(), X) << " ";
     }
     return 0;
-}
-
-int lower_bound(vector<int>& v, int target){
-    int low = 0;
-    int high = v.size()- 1;
-    int mid;
-    while(low <= high){
-        mid = (low + high)/2;
-        if(v[mid] < target){
-            low = mid + 1;
-        }
-        else high = mid - 1;
-    }
-    return low;
-}
-
-int upper_bound(vector<int>& v, int target){
-    int low = 0;
-    int high = v.size()- 1;
-    int mid;
-    while(low <= high){
-        mid = (low + high)/2;
-        if(v[mid] <= target){
-            low = mid + 1;
-        }
-        else high = mid - 1;
-    }
-    return low;
 }
